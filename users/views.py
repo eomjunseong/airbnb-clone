23df5702ff82ca179116d2cfe29e631084892e84
eomjunseong <66054183+eomjunseong@users.noreply.github.com>
2021-05-04 16:19:31 +0900

    14.0

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        pass
+
+
+# 위 클래스는 아래와 같음
+# def login_view(requset):
+#    if requset.method=="GET":
+#        pass
+#    elif requset.method=="POST":
