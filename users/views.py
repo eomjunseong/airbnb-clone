4d05101bd60727ce05351d8f1734ea37cb6f7b56
eomjunseong <66054183+eomjunseong@users.noreply.github.com>
2021-05-04 17:50:53 +0900

    14.4

--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 class LoginView(View):
     def get(self, request):
 
-        form = forms.LoginForm(initial={"email": "itn@las.com"})
+        form = forms.LoginForm(initial={"email": "um1129@naver.com"})
 
         return render(request, "users/login.html", {"form": form})
 
@@ -15,7 +16,12 @@ class LoginView(View):
         form = forms.LoginForm(request.POST)
 
         if form.is_valid():  # is_valid : LoginForm의 clean___: check
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
 
 
@@ -24,3 +30,9 @@ class LoginView(View):
 #    if requset.method=="GET":
 #        pass
 #    elif requset.method=="POST":
+
+
+# 로그아웃 무조건 이렇게
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
